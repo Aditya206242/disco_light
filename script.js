97a8a7e58d99549b0476e5ae23452f0197292b78
Your Name <your.ak008619@gmail.com>
2023-12-15 20:08:56 +0530

    updated

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ function getRandColor(){
 // boxes ka array banana 
 Array.from(boxes).forEach(e=>{
     console.log(e);
-    e.style.backgroundColor= getRandColor();
+    
 
     
 })
 
-setInterval(function () {
-    Array.from(boxes).forEach(e => {
-        e.style.backgroundColor = getRandColor();
-    });
-}, 500);
+// setInterval(function () {
+//     Array.from(boxes).forEach((e) => {
+//         e.style.backgroundColor = getRandColor();
+//     });
+// }, 500);
 
 
 
@@ -32,4 +32,67 @@ let b = 255; // so here we are going to genarate random number between 0 to 255
 
 let random = a + Math.random() *(b-a);
 console.log(random);  // this concept we will use in above code to get random color
-  
+
+
+// for start and stop button code
+function play_pause(){
+    if (audioPlayer.paused) {
+    audioPlayer.play();
+    playPauseButton.textContent = 'Pause';
+    } else {
+    audioPlayer.pause();
+    playPauseButton.textContent = 'Play';
+    }
+}
+
+let startBtn =document.getElementById("start-btn");
+startBtn.addEventListener("click",()=>{
+   a = setInterval(function () {
+        Array.from(boxes).forEach((e) => {
+            e.style.backgroundColor = getRandColor();
+        });
+    }, 400);
+
+    audioPlayer.play();
+    
+   
+    
+    
+})
+
+
+let stopBtn =document.getElementById("stop-btn");
+stopBtn.addEventListener("click",()=>{
+    clearInterval(a);
+    audioPlayer.pause();
+    
+    
+})
+
+let closeLight=document.getElementById("close-light");
+
+closeLight.addEventListener("click",()=>{
+
+    let boxs =document.getElementsByClassName("box");
+    
+
+   clearInterval(a);
+   for(var i =0;i<boxs.length;i++){
+    boxs[i].style.backgroundColor="black";
+   }
+   audioPlayer.pause();
+})
+
+// code for audio 
+var audioPlayer = document.getElementById('audioPlayer');
+var playPauseButton = document.getElementById('playPauseButton');
+
+playPauseButton.addEventListener('click', function() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        playPauseButton.textContent = 'Pause';
+    } else {
+        audioPlayer.pause();
+        playPauseButton.textContent = 'Play';
+    }
+});
